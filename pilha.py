faaea05053ecbe3df1419efd4eafec014f29d8e0
vinicius <vinicius.franco@alu.unibalsas.edu.br>
2024-03-26 19:45:39 -0300

    segnda att

--- pilha.py
+++ pilha.py
@@ -1,4 +1,10 @@
 class stack:
     def __init__(self) :
         self.items = []
-        
+    
+    def isEmpty (self):
+        return len(self.items) == 0
+    
+    def push(self, item):
+        self.items.append(item)
+        print(f'item: {item} foi colocado na lista')
